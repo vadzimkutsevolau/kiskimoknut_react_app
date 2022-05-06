7007cd03262dbf8dafd690ad1404b5978dce53dc
vadzimkutsevolau <v.kutsevolau@gmail.com>
2022-05-06 16:47:50 +0400

    Init. Sorted by function reailized

--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+// import Counter from "./components/Counter";
+// import ClassCounter from "./components/ClassCounter";
+import './styles/App.css';
+// import PostItem from "./components/PostItem";
+import PostList from "./components/PostList";
+// import MyButton from "./components/UI/button/MyButton";
+import MyInput from "./components/UI/input/MyInput";
+import PostForm from './components/PostForm';
+import MySelect from './components/UI/select/MySelect';
 
 function App() {
+  const [posts, setPosts] = useState([
+    {id: 1, title: 'Ruby', body: "Description"},
+    {id: 2, title: 'Ruby2', body: "Description2"},
+    {id: 3, title: 'Ruby3', body: "Description3"},
+  ])
+  const [selectedSort, setSelectedSort] = useState('')
+  const [searchQuery, setSearchQuery] = useState('')
+
+  function getSortedPosts() {
+    if(selectedSort) {
+      return [...posts].sort((a, b) => a[selectedSort].localCompare(b[selectedSort]))
+    }
+    return posts;
+  }
+
+  const sortedPosts = getSortedPosts()
+
+  const createPost = (newPost) => {
+    setPosts([...posts, newPost])
+  }
+
+  const removePost = (post) => {
+    setPosts(posts.filter(p => p.id !== post.id))
+  }
+
+  const sortPosts = (sort) => {
+    setSelectedSort(sort)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <PostForm create={createPost}/>
+      <hr style={{margin: '15px 0'}}/>
+      <div>
+          <MyInput
+              value={searchQuery}
+              onChange={e => setSearchQuery(e.target.value)}
+              placeholder='Search...'
+          />
+          <MySelect
+              value={selectedSort}
+              onChange={sortPosts}
+              defaultValue="Sort by"
+              options={[
+                {value: 'title', name: 'By title'},
+                {value: 'body', name: 'By desciption'}
+              ]}
+          />
+      </div>
+      {posts.length
+        ? 
+        <PostList remove={removePost} posts={sortedPosts} title="Ruby posts"/>
+        : 
+        <h1 style={{textAlign: 'center'}}>
+          No posts found :(
+        </h1>
+      }
     </div>
   );
 }
